57c52409949afcd3b99e433276d1e89dc33c1f31
Moise Nguala <tsimbanguala150@gmail.com>
2026-01-12 07:33:59 +0100

    correction de typages typescript

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
-import Footer from "./components/Common/Footer /Footer";
-import NavBar from "./components/Common/NavBar /NavBar";
+import Footer from "./components/Common/Footer/Footer";
+import NavBar from "./components/Common/NavBar/NavBar";
 import ScrollToTop from "./components/Common/ScrollToTop/ScrollToTop";
 export default function RootLayout({
   children,
