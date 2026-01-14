0629d1f1b38e9fea83151c6b68587962690b7e9d
Moise Nguala <tsimbanguala150@gmail.com>
2026-01-14 20:01:43 +0100

    remove folder in common

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import Footer from "./components/Common/Footer/Footer";
-import NavBar from "./components/Common/NavBar/NavBar";
-import ScrollToTop from "./components/Common/ScrollToTop/ScrollToTop";
+import Footer from "./components/Common/Footer";
+import NavBar from "./components/Common/NavBar";
+import ScrollToTop from "./components/Common/ScrollToTop";
 import "./globals.css";
 
 export default function RootLayout({
